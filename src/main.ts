22b7f872e8883ad056a953125190dab44fb4473c
MuFeng <2563247404@qq.com>
2023-04-27 14:34:35 +0800

    删除所有的冗余代码，新增Loading页面

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,8 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
-import router from './router'
-
-import './assets/main.css'
-
 const app = createApp(App)
-
 app.use(createPinia())
-app.use(router)
-
+console.log("test")
+await new Promise(resolve => setTimeout(resolve, 1200))
 app.mount('#app')
