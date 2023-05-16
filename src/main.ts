bc6b3dcc18d952d57dfb122f7f0d53878d38323c
潘纪婷 <mango@imufeng.cn>
2023-05-16 16:47:35 +0800

    增加全居回到顶部方法

--- src/main.ts
+++ src/main.ts
@@ -29,6 +29,25 @@ Viewer.setDefaults({
     next: true
   }
 })
+//滚动至页面顶部，使用 Element-Plus 回到顶部 组件中的算法
+const cubic = (value: number) => Math.pow(value, 3);
+const easeInOutCubic = (value: number) => value < 0.5 ? cubic(value * 2) / 2 : 1 - cubic((1 - value) * 2) / 2;
+app.config.globalProperties.scrollToTop = function () {
+  const el = document.documentElement
+  const beginTime = Date.now()
+  const beginValue = el.scrollTop
+  const rAF = window.requestAnimationFrame || (func => setTimeout(func, 16))
+  const frameFunc = () => {
+    const progress = (Date.now() - beginTime) / 500;
+    if (progress < 1) {
+      el.scrollTop = beginValue * (1 - easeInOutCubic(progress))
+      rAF(frameFunc)
+    } else {
+      el.scrollTop = 0
+    }
+  }
+  rAF(frameFunc)
+}
 // 注册element Icons组件
 Object.keys(Icons).forEach(key => {
   app.component(key, Icons[key as keyof typeof Icons]);
